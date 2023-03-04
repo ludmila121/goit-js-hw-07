6157f29a6c8691baef390cf69e29f9f3f2dd1912
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-03-04 22:01:36 +0100

    111

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,6 +7,29 @@
 // Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 
 import { galleryItems } from './gallery-items.js';
-const galeryImg = document.querySelector(`gallery`);
-const markUp = createMarkeUp(galleryItems);
+ 
+const galeryImg = document.querySelector(`.gallery`);
+galeryImg.insertAdjacentHTML(`beforeend`, createMarkeUp);
+function createMarkeUp(galleryItems) {
+    const markUp = galleryItems.map(({ preview, original,description}) => {
+        return `<div><a><img src = "${preview}" title =" " alt =" "></a></div>`;
+    }).join(``);
+    return markUp;
+}
 console.log(galleryItems);
+
+
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+        <input placeholder="Type something">
+        <a>Close</a>
+    </div>
+`, {
+    onShow: (instance) => {
+        instance.element().querySelector('a').onclick = instance.close
+    }
+})
+
+instance.show()

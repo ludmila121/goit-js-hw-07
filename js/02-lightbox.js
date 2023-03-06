a1670f0c16783433e16be63105d03d7f2ea9f6a7
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-03-06 18:40:48 +0100

    777

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryListRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const makeGalleryListMarkup = ({ original, preview, description}) => {
+  return `
+     <ul class="gallery"><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></ul>
+  `;
+};
+
+const makeGalleryListElement = galleryItems.map(makeGalleryListMarkup).join("");
+galleryListRef.insertAdjacentHTML("beforeend", makeGalleryListElement);
+
+galleryListRef.addEventListener("click", onGalleryImageClick);
+
+function onGalleryImageClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionPosition: 'bottom', captionDelay: 250 });
+}
+
